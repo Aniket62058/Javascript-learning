70e2160fd53e8d437e55a2134e66fff699cbedec
Aniket Shandilya <aniket62058@gmail.com>
2023-09-16 13:04:55 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 2) //we are using NodeJS not browser
+
+console.log( 2 
+    + 8 ) // code readability should be high
+
+console.log("Aniket")
+
+let name="Aniket"
+let age=20
+let isLoggedIn=true
+let state
+
+// number => 2 to power 53
+// bigInt
+// string ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
